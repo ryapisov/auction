9c9ceb4430168f1ed381ba2f2f407bf394181a57
Alexey V.Ryapisov <tealikes@yandex.ru>
2020-12-31 18:05:57 +0300

    add module react-player

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import { DatePicker } from 'antd'
+import { DatePicker,  Button } from 'antd'
+import ReactPlayer from 'react-player/youtube'
 
 function App() {
   return (
     <div className="test">
+      <ReactPlayer 
+        className=''
+        url='https://www.youtube.com/watch?v=Sl9ac5BJxfs' 
+        valume={0}
+        muted={true}
+        width='100wh'
+        height='100vh'
+        // light={true}
+        onReady={true}
+        loop={true}
+        playing={true}
+      />
+      <Button type="primary">PRESS ME</Button>
       <DatePicker />- аукцион
     </div>
   )
