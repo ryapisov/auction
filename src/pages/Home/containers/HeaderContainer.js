fe21956da15b3740d8623b12fd5707c7c173a62f
Alexey V.Ryapisov <tealikes@yandex.ru>
2021-01-03 16:29:16 +0300

    refactor header

--- src/pages/Home/containers/HeaderContainer.js
+++ src/pages/Home/containers/HeaderContainer.js
@@ -1,38 +1,52 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Layout, Space, Row, Col, Divider, Typography } from 'antd'
-const { Header } = Layout
-const { Title } = Typography
+import { Layout, Space, Row, Col, Typography } from 'antd'
 
-const DemoBox = props => <p className={`height-${props.value}`}>{props.children}</p>
-
-const HeaderStyle = styled(Header)`
+const Header= styled(Layout.Header)`
   height: 30vh;
   background-color: hsla(247, 93%, 67%, 1.0);
   color: #fff;
   z-index: 1;
 `
-
+const SpaceItem = styled.span`
+  display:inline-block;
+  text-align: center;
+  font-size: 1.1rem;
+  font-weight:600;
+  margin-right:112px;
+  :hover{
+    color: #eee;
+    cursor:pointer;
+  }
+`
+const Title = styled(Typography)`
+  text-align: center;
+  margin-top: 15px;
+  font-size: 38px;
+  font-weight:600;
+  color: #fff;
+`
 const HeaderContainer = () => {
   return(
-    <HeaderStyle>
+    <Header>
       <Row style={{height:'15vh'}} justify="center" align="bottom">
         <Col span={8}>
-          <Title level={2} style={{textAlign:'center', marginTop:'15px', color:'#fff'}}>
-            Аукцион
-          </Title>
+          <Title>Аукцион</Title>
         </Col>
       </Row>
+
       <Row style={{height:'15vh'}} justify="left" align="bottom">
-        <Col span={18} style={{background:'', marginBottom:'-12px'}}>
-          <Space style={{background:'', padding:'0'}}>
-            <Title level={5} style={{textAlign:'center', marginLeft:'60px', marginTop:'0px', color:'#fff'}}> Link </Title>
-            <Title level={5} style={{textAlign:'center', marginLeft:'80px', marginTop:'0px', color:'#fff'}}> Link </Title>
-            <Title level={5} style={{textAlign:'center', marginLeft:'80px', marginTop:'0px', color:'#fff'}}> Демо аккаунт </Title>
+        <Col span={18} style={{marginBottom:'-10px'}}>
+          <Space>
+            <SpaceItem onClick={()=>alert(1)}>аукцион</SpaceItem>
+            <SpaceItem onClick={()=>alert(2)}>товары</SpaceItem>
+            <SpaceItem onClick={()=>alert(3)}>карьера</SpaceItem>
+            <SpaceItem onClick={()=>alert(4)}>заработок</SpaceItem>
+            <SpaceItem onClick={()=>alert(5)}>демо&nbsp;аккаунт</SpaceItem>
           </Space>
         </Col>
       </Row>
-    </HeaderStyle>
+    </Header>
   )
 }
 
